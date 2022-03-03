8036e3db1835f57ccec4278a9a478f771f665b80
Phong Hoang <phonght@smartosc.com>
2022-03-03 18:42:00 +0700

    add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,15 @@ import { defineNuxtConfig } from "nuxt3";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  meta: {
+    link: [
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/favicon.ico",
+      },
+    ],
+  },
   css: ["~/assets/scss/global.scss"],
   buildModules: ["@nuxtjs/tailwindcss", ["@pinia/nuxt", { disableVuex: true }]],
   components: ["~/components/common", "~/components/ui"],
